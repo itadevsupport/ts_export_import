d7172fed3309f223914c66c8af2b1651706428be
daniepa <daniloepatti@gmail.com>
2022-11-03 15:25:57 +0100

    first commit: esempi post js ts export import del 3/11/22

---
+++ src/script/app.ts
@@ -0,0 +1,4 @@
+import { somma, moltiplicazione } from "./utils.js";
+
+console.log(somma(5,10));
+console.log(moltiplicazione(5,10));
---
+++ src/script/utils.ts
@@ -0,0 +1,9 @@
+function somma(num1: number, num2: number): string {
+    return `la somma di ${num1} e ${num2} è ${num1 + num2}`;
+}
+
+function moltiplicazione(num1: number, num2: number): string {
+    return `il prodotto di ${num1} e ${num2} è ${num1 * num2}`;
+}
+
+export { somma, moltiplicazione };
